9126533adea94d9f8a25f72616a2aa6d75979a4a
dennyswamb <dennyswamb@localhost.localdomain>
2024-12-11 16:59:17 +0300

    second5 commit

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Disable GPU usage
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 # Debugging: Check if the model directory exists and list its contents
 model_dir = "saved_model"
 if not os.path.exists(model_dir):
@@ -44,4 +47,5 @@ def predict():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
